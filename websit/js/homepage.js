938b1a792527a46e5792e5633a555c781f8e1208
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2022-09-30 19:52:39 +0200

    Update homepage.js

--- websit/js/homepage.js
+++ websit/js/homepage.js
@@ -5,73 +5,209 @@ var xhr = new XMLHttpRequest();
 
 var selectCity;
 
-var city;
+var city_name;
+var citys_weather_data = [];
 
 $(document).ready(function(){ 
 	
 	selectCity = document.getElementById('selectCity');
 	
-	city = selectCity.value;
+	city_name = selectCity.value;
 	
-	updateData();
+	getDataByCity(city_name);
 });
 
 $(document).ready(function(){
 	$("#selectCity").change(function(){
-		city = selectCity.value;
-		console.log("[select city] "+city);
-		updateData();
+		city_name = selectCity.value;
+		console.log("[select city] "+city_name);
+		getDataByCity(city_name);
 	});
 });
 
-function updateData()
+function updateData(weather_datas,city_name)
 {
-	getDataByCity();
+	var n = 0;
+	var single_data = createWeatherData(-1);
+	var time = -1;
+	if (city_name in citys_weather_data){
+		time = citys_weather_data[city_name].at(-1).getTime().toISOString();
+		var start = false;
+		for (var i = 0; i < weather_datas.length; i++){
+			data = weather_datas[i]
+			if (time != data.time){
+				if (start){
+					time = data.time;
+					if (single_data.getTime().toISOString() != (new Date(-1)).toISOString()) {
+						citys_weather_data[city_name].push(single_data);
+						n+=1;
+					} 
+					single_data = createWeatherData(time);
+				}
+			}
+			else {
+				start = true;
+			}
+			if (start){
+				single_data.setData(data);
+			}
+		}
+	}
+	else {		
+		citys_weather_data[city_name] = [];
+		for (var i = 0; i < weather_datas.length; i++){
+			data = weather_datas[i];
+			if (time != data.time){
+				if (single_data.getTime().toISOString() != (new Date(-1)).toISOString()){
+					citys_weather_data[city_name].push(single_data);
+					n+=1;
+				}
+				time = data.time;
+				single_data = createWeatherData(time);
+			}
+			single_data.setData(data);
+		}
+	}
+	console.log("[update city weather data] new num: "+n)
+	console.log(citys_weather_data)
 }
 
-function getDataByCity()
+function getDataByCity(city_name)
 {
-	url = host + 'data/' + city;
+	url = host + 'data/' + city_name;
 	console.log("[get data] url: "+url);
-	xhr.open("GET", url, false);
+	xhr.open("GET", url, true);
 	xhr.onreadystatechange = function() {
     	if (xhr.readyState == XMLHttpRequest.DONE) {
         	//alert();
-			console.log("[GET city data successed] "+city);
-			weather = JSON.parse(xhr.responseText)
-			console.log(weather)
+			console.log("[GET city weather data successed] "+city_name);
+			weather_datas = JSON.parse(xhr.responseText);
+			updateData(weather_datas,city_name);
     	}
 	}
-	console.log(xhr.disconnect)
 	xhr.send();
 }
 
 //[object]
-const WeatherData = function(time){
+const createWeatherData = function(time){
+	
     const weather_data = {}
     weather_data.temperature = 0;
     weather_data.precipitation = 0;
 	weather_data.wind_speed = 0;
 	weather_data.cloud_coverage = 0;
-	weather_data.time = time;
+	weather_data.time = new Date(time);
 	
-
-    point.getX = function(){
-        return point.x
+    weather_data.getTime = function(){
+        return weather_data.time
     }
+	
+	weather_data.getTemperature = function(){
+		return weather_data.temperature
+	}
+	
+	weather_data.setTemperature = function(data){
+		weather_data.temperature = createDataValue(data.value,data.unit)
+	}
+	
+	weather_data.getPrecipitation = function(){
+		return weather_data.precipitation
+	}
+	
+	weather_data.setPrecipitation = function(data){
+		weather_data.precipitation = createPrecipitationDataValue(data.value,data.unit,data.precipitation_type)
+	}
+	
+	weather_data.getWindSpeed = function(){
+		return weather_data.wind_speed
+	}
+	
+	weather_data.setWindSpeed = function(data){
+		weather_data.wind_speed = createWindSpeedDataValue(data.value,data.unit,data.direction)
+	}
+	
+	weather_data.getCloudCoverage = function(){
+		return weather_data.cloud_coverage
+	}
+	
+	weather_data.setCloudCoverage = function(data){
+		weather_data.cloud_coverage = createDataValue(data.value,data.unit)
+	}
+	
+	weather_data.setData = function(data){
+		switch(data.type) {
+			case "temperature":
+				weather_data.setTemperature(data)
+				break
+			case "precipitation":
+				weather_data.setPrecipitation(data)
+				break
+			case "wind speed":
+				weather_data.setWindSpeed(data)
+				break
+			case "cloud coverage":
+				weather_data.setCloudCoverage(data)
+				break
+			default:
+				break
+		}
+	}
+	
+    return weather_data
+}
 
-    point.getY = function(){
-        return point.y
-    }
+const createWindSpeedDataValue = function(value,unit,direction){
+	const data_value = {}
+	data_value.direction = direction;
+	Object.setPrototypeOf(data_value,createDataValue(value,unit))
+	
+	data_value.getDirection = function(){
+		return data_value.direction
+	}
+	
+	data_value.setDirection = function(direction){
+		data_value.direction = direction
+	}
+	
+	return data_value
+}
 
-    point.moveTo = function(x,y){
-        point.x = x
-        point.y = y
-    }
+const createPrecipitationDataValue = function(value,unit,precipitation_type){
+	const data_value = {}
+	data_value.precipitation_type = precipitation_type;
+	Object.setPrototypeOf(data_value,createDataValue(value,unit))
+	
+	data_value.getPrecipitationType = function(){
+		return data_value.precipitation_type
+	}
+	
+	data_value.setPrecipitationType = function(precipitation_type){
+		data_value.precipitation_type = precipitation_type
+	}
+	
+	return data_value
+}
 
-    point.toString = function(){
-        return 'x:'+point.x+' y:'+point.y
-    }
-
-    return point
+const createDataValue = function(value,unit){
+	const data_value = {}
+	data_value.value = value;
+	data_value.unit = unit;
+	
+	data_value.getValue = function(){
+		return data_value.value
+	}
+	
+	data_value.getUnit = function(){
+		return data_value.unit
+	}
+	
+	data_value.setValue = function(value){
+		data_value.value = value
+	}
+	
+	data_value.setUnit = function(unit){
+		data_value.unit = unit
+	}
+	
+	return data_value
 }
