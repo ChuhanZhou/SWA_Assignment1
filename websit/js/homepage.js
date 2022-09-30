d1392e745a26bee25d6684aaf30990f41fb6ed34
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2022-09-30 17:07:52 +0200

    Add files via upload

---
+++ websit/js/homepage.js
@@ -0,0 +1,77 @@
+// JavaScript Document
+
+var host = 'http://localhost:8080/'
+var xhr = new XMLHttpRequest();
+
+var selectCity;
+
+var city;
+
+$(document).ready(function(){ 
+	
+	selectCity = document.getElementById('selectCity');
+	
+	city = selectCity.value;
+	
+	updateData();
+});
+
+$(document).ready(function(){
+	$("#selectCity").change(function(){
+		city = selectCity.value;
+		console.log("[select city] "+city);
+		updateData();
+	});
+});
+
+function updateData()
+{
+	getDataByCity();
+}
+
+function getDataByCity()
+{
+	url = host + 'data/' + city;
+	console.log("[get data] url: "+url);
+	xhr.open("GET", url, false);
+	xhr.onreadystatechange = function() {
+    	if (xhr.readyState == XMLHttpRequest.DONE) {
+        	//alert();
+			console.log("[GET city data successed] "+city);
+			weather = JSON.parse(xhr.responseText)
+			console.log(weather)
+    	}
+	}
+	console.log(xhr.disconnect)
+	xhr.send();
+}
+
+//[object]
+const WeatherData = function(time){
+    const weather_data = {}
+    weather_data.temperature = ;
+    weather_data.precipitation = ;
+	weather_data.wind_speed = ;
+	weather_data.cloud_coverage = ;
+	weather_data.time = time;
+	
+
+    point.getX = function(){
+        return point.x
+    }
+
+    point.getY = function(){
+        return point.y
+    }
+
+    point.moveTo = function(x,y){
+        point.x = x
+        point.y = y
+    }
+
+    point.toString = function(){
+        return 'x:'+point.x+' y:'+point.y
+    }
+
+    return point
+}
