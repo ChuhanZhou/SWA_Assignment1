7889dfdc6ea9c96b0a73b28b277cf62d27392675
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2022-09-30 22:47:23 +0200

    Update homepage.js

--- websit/js/homepage.js
+++ websit/js/homepage.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
 	city_name = selectCity.value;
 	
 	getDataByCity(city_name);
+	
 });
 
 $(document).ready(function(){
@@ -27,19 +28,26 @@ $(document).ready(function(){
 
 function updateData(weather_datas,city_name)
 {
-	var n = 0;
-	var single_data = createWeatherData(-1);
-	var time = -1;
-	if (city_name in citys_weather_data){
-		time = citys_weather_data[city_name].at(-1).getTime().toISOString();
+	var n = 0
+	var time = -1
+	var date = "-1"
+	var daily_data = createDailyWeatherData(date)
+	var single_data = createWeatherData(time)
+
+	if (city_name in citys_weather_data && citys_weather_data[city_name].length>0){
+		time = citys_weather_data[city_name].at(-1).getWeatherDatas().at(-1).getTime().toISOString();
+		date = citys_weather_data[city_name].at(-1).getDate()
+		daily_data = citys_weather_data[city_name].at(-1)
+		console.log(time,date)
 		var start = false;
 		for (var i = 0; i < weather_datas.length; i++){
 			data = weather_datas[i]
+			data_date = (new Date(data.time)).toLocaleDateString()
 			if (time != data.time){
 				if (start){
 					time = data.time;
 					if (single_data.getTime().toISOString() != (new Date(-1)).toISOString()) {
-						citys_weather_data[city_name].push(single_data);
+						daily_data.getWeatherDatas().push(single_data);
 						n+=1;
 					} 
 					single_data = createWeatherData(time);
@@ -48,24 +56,53 @@ function updateData(weather_datas,city_name)
 			else {
 				start = true;
 			}
+
+			if (date != data_date && start){
+				if (daily_data.getDate() != "-1"){
+					date = data_date
+					daily_data = createDailyWeatherData(date)
+					citys_weather_data[city_name].push(daily_data);
+				}
+			}
+
 			if (start){
 				single_data.setData(data);
 			}
+		}
+		if (single_data.getTime().toISOString() != (new Date(-1)).toISOString()){
+			daily_data.getWeatherDatas().push(single_data)
+			n+=1
 		}
 	}
 	else {		
 		citys_weather_data[city_name] = [];
 		for (var i = 0; i < weather_datas.length; i++){
 			data = weather_datas[i];
+			data_date = (new Date(data.time)).toLocaleDateString()
+
 			if (time != data.time){
 				if (single_data.getTime().toISOString() != (new Date(-1)).toISOString()){
-					citys_weather_data[city_name].push(single_data);
+					daily_data.getWeatherDatas().push(single_data);
 					n+=1;
 				}
 				time = data.time;
 				single_data = createWeatherData(time);
 			}
+
+			if (date != data_date){
+				if (daily_data.getDate() != "-1"){
+					citys_weather_data[city_name].push(daily_data);
+				}
+				date = data_date
+				daily_data = createDailyWeatherData(date)
+			}
+
 			single_data.setData(data);
+		}
+		if (single_data.getTime().toISOString() != (new Date(-1)).toISOString()){
+			daily_data.getWeatherDatas().push(single_data)
+			citys_weather_data[city_name].push(daily_data)
+			n+=1
 		}
 	}
 	console.log("[update city weather data] new num: "+n)
@@ -83,12 +120,116 @@ function getDataByCity(city_name)
 			console.log("[GET city weather data successed] "+city_name);
 			weather_datas = JSON.parse(xhr.responseText);
 			updateData(weather_datas,city_name);
+			console.log(getMinTemperature(city_name,"2022/9/28"))
+			console.log(getMaxTemperature(city_name,"2022/9/28"))
+			console.log(getTotalPrecipitation(city_name,"2022/9/28"))
+			console.log(getAverageWindSpeed(city_name,"2022/9/28"))
     	}
 	}
 	xhr.send();
 }
 
+function getMinTemperature(city_name,date){
+	city_weather_data = citys_weather_data[city_name]
+	min_temp = undefined
+	for (var i = city_weather_data.length-1; i >=0; i--){
+		date_data = city_weather_data[i]
+		if (date_data.getDate()==date){
+			for(var d_i = 0; d_i < date_data.getWeatherDatas().length; d_i++){
+				temp = date_data.getWeatherDatas()[d_i].getTemperature()
+				if (min_temp==undefined || temp.getValue()<min_temp.getValue()){
+					min_temp = temp
+				}
+			}
+			break
+		}
+	}
+	return min_temp
+}
+
+function getMaxTemperature(city_name,date){
+	city_weather_data = citys_weather_data[city_name]
+	max_temp = undefined
+	for (var i = city_weather_data.length-1; i >=0; i--){
+		date_data = city_weather_data[i]
+		if (date_data.getDate()==date){
+			for(var d_i = 0; d_i < date_data.getWeatherDatas().length; d_i++){
+				temp = date_data.getWeatherDatas()[d_i].getTemperature()
+				if (max_temp==undefined || temp.getValue()>max_temp.getValue()){
+					max_temp = temp
+				}
+			}
+			break
+		}
+	}
+	return max_temp
+}
+
+function getTotalPrecipitation(city_name,date){
+	city_weather_data = citys_weather_data[city_name]
+	total_precipitation = undefined
+	for (var i = city_weather_data.length-1; i >=0; i--){
+		date_data = city_weather_data[i]
+		if (date_data.getDate()==date){
+			for(var d_i = 0; d_i < date_data.getWeatherDatas().length; d_i++){
+				precipitation = date_data.getWeatherDatas()[d_i].getPrecipitation()
+				if (total_precipitation==undefined){
+					total_precipitation = precipitation.getDataValue().copy()
+				}
+				else
+				{
+					total_precipitation.setValue(total_precipitation.getValue()+precipitation.getValue())
+				}
+			}
+			break
+		}
+	}
+	return total_precipitation
+}
+
+function getAverageWindSpeed(city_name,date){
+	city_weather_data = citys_weather_data[city_name]
+	average_wind_speed = undefined
+	for (var i = city_weather_data.length-1; i >=0; i--){
+		date_data = city_weather_data[i]
+		if (date_data.getDate()==date){
+			for(var d_i = 0; d_i < date_data.getWeatherDatas().length; d_i++){
+				wind_speed = date_data.getWeatherDatas()[d_i].getWindSpeed()
+				if (average_wind_speed==undefined){
+					average_wind_speed = wind_speed.getDataValue().copy()
+				}
+				else
+				{
+					average_wind_speed.setValue(average_wind_speed.getValue()+wind_speed.getValue())
+				}
+			}
+			average_wind_speed.setValue(average_wind_speed.getValue()/date_data.getWeatherDatas().length)
+			break
+		}
+	}
+	return average_wind_speed
+}
+
 //[object]
+const createDailyWeatherData = function(date){
+	const daily_weather_data = {}
+	daily_weather_data.date = date
+	daily_weather_data.weather_datas = []
+
+	daily_weather_data.getDate = function(){
+        return daily_weather_data.date
+    }
+
+	daily_weather_data.getWeatherDatas = function(){
+        return daily_weather_data.weather_datas
+    }
+
+	daily_weather_data.setWeatherDatas = function(weather_datas){
+        daily_weather_data.weather_datas = weather_datas
+    }
+	return daily_weather_data
+}
+
 const createWeatherData = function(time){
 	
     const weather_data = {}
@@ -168,6 +309,10 @@ const createWindSpeedDataValue = function(value,unit,direction){
 	data_value.setDirection = function(direction){
 		data_value.direction = direction
 	}
+
+	data_value.copy = function(){
+		return createWindSpeedDataValue(value,unit,direction)
+	}
 	
 	return data_value
 }
@@ -183,6 +328,10 @@ const createPrecipitationDataValue = function(value,unit,precipitation_type){
 	
 	data_value.setPrecipitationType = function(precipitation_type){
 		data_value.precipitation_type = precipitation_type
+	}
+
+	data_value.copy = function(){
+		return createPrecipitationDataValue(value,unit,precipitation_type)
 	}
 	
 	return data_value
@@ -207,6 +356,14 @@ const createDataValue = function(value,unit){
 	
 	data_value.setUnit = function(unit){
 		data_value.unit = unit
+	}
+
+	data_value.getDataValue = function(){
+		return data_value
+	}
+
+	data_value.copy = function(){
+		return createDataValue(value,unit)
 	}
 	
 	return data_value
